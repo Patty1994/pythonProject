9bbcc44bb6dbcbf3dc094b4153f60fae7ddc9957
prateek <prateekthapliyal1994@gmail.com>
2022-01-03 21:56:19 -0800

    new projects added

--- music_player.py
+++ music_player.py
@@ -7,7 +7,6 @@ music_player = tkr.Tk()
 music_player.title("My Music Player")
 music_player.geometry("450x350")
 directory = askdirectory()
-os.chdir(directory)
 song_list = os.listdir()
 
 play_list = tkr.Listbox(music_player, font="Helvetica 12 bold", bg='yellow', selectmode=tkr.SINGLE)
